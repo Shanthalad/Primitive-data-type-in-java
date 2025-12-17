852e891f555b6202ae24efb7d49f09dcb924184a
Lenovo <Lenovo@DESKTOP-PNAR7AQ>
2025-12-17 19:59:04 +0530

    uses of Primitive data types in java

---
+++ src/com/edubridge/MyPrimitive.java
@@ -0,0 +1,25 @@
+package com.edubridge;
+
+public class MyPrimitive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int i = 100;
+        System.out.println(i);
+        float s = 3.14f;
+        System.out.println(s);
+        char c = 's';
+        System.out.println(c);
+        long phone=901942121;
+        System.out.println(phone);
+        boolean b= true;
+        System.out.println(b);
+        double d= 9.0;
+        System.out.println(d);
+        String name ="java";
+        System.out.println(name);
+        
+        
+	}
+
+}
